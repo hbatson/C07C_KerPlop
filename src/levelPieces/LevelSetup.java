fa355fb5be866218f2354cd44ac6e50f09ed7d08
Benjamin Kaylor <blkaylor22@gmail.com>
2024-02-05 20:26:55 -0700

    Game pieces now move and interact

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -4,32 +4,32 @@ import gameEngine.Moveable;
 import java.util.ArrayList;
 
 public class LevelSetup {
-    private int levelNum;
     private gameEngine.Drawable[] board;
     private ArrayList<Moveable> movingPieces;
     private ArrayList<GamePiece> interactingPieces;
     private int playerStartLoc;
     
-    public LevelSetup() {
-    }
-
     public void createLevel(int levelNum) {
-        this.levelNum = levelNum;
         this.board = new gameEngine.Drawable[gameEngine.GameEngine.BOARD_SIZE];
         this.movingPieces = new ArrayList<Moveable>();
         this.interactingPieces = new ArrayList<GamePiece>();
         System.out.println("Level " + levelNum + " Legend:");
         System.out.println(" P - Player (you)");
-        this.playerStartLoc = 11;
+        this.playerStartLoc = 10;
         if (levelNum == 1) {
             Owl owl = new levelPieces.Owl(0);
             board[0] = owl;
+            interactingPieces.add(owl);
+            movingPieces.add(owl);
             Tree tree = new levelPieces.Tree(5);
             board[5] = tree;
             Wizard wizard = new levelPieces.Wizard(15);
             board[15] = wizard;
+            interactingPieces.add(wizard);
+            movingPieces.add(wizard);
             Ring ring = new levelPieces.Ring(1);
             board[1] = ring;
+            interactingPieces.add(ring);
             System.out.println(" " + owl);
             System.out.println(" " + tree);
             System.out.println(" " + wizard);
@@ -38,12 +38,16 @@ public class LevelSetup {
         } else if (levelNum == 2) {
             Cannon cannon = new levelPieces.Cannon(0);
             board[0] = cannon;
+            interactingPieces.add(cannon);
             Groundhog groundhog = new levelPieces.Groundhog(11);
             board[11] = groundhog;
+            interactingPieces.add(groundhog);
+            movingPieces.add(groundhog);
             Tree tree = new levelPieces.Tree(5);
             board[5] = tree;
             Ring ring = new levelPieces.Ring(20);
             board[20] = ring;
+            interactingPieces.add(ring);
             System.out.println(" " + cannon);
             System.out.println(" " + groundhog);
             System.out.println(" " + tree);
