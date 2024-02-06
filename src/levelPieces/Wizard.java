d976d47bd8f814ba5a9695d50e96d22ba0d19dad
Benjamin Kaylor <blkaylor22@gmail.com>
2024-02-05 20:57:52 -0700

    Fixed bug where wizard cloned when moving

--- src/levelPieces/Wizard.java
+++ src/levelPieces/Wizard.java
@@ -5,12 +5,20 @@ import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
 import gameEngine.Moveable;
 
+/**
+ * Wizard is a GamePiece that moves away from the player and gives them a point if they land on it
+ *
+ * @author Henry Batson
+ * @author Benjamin Kaylor
+ *
+ */
 public class Wizard extends GamePiece implements Moveable{
 
 	public Wizard(int location) {
 		super('W', "Wizard - (Special character)", location);
 	}
 
+    // gives the player a point if they land on the wizard
 	@Override
 	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
         if (playerLocation == this.getLocation()) {
@@ -20,6 +28,7 @@ public class Wizard extends GamePiece implements Moveable{
         }
 	}
 
+    // moves away from the player when close
 	@Override
 	public void move(Drawable[] gameBoard, int playerLocation) {
 		int newLocation = getLocation();
@@ -32,6 +41,7 @@ public class Wizard extends GamePiece implements Moveable{
 				newLocation = getLocation() +1;
 			}
 		}
+        gameBoard[getLocation()] = null;
 		setLocation(newLocation);
 		gameBoard[newLocation] = this;
 	}
